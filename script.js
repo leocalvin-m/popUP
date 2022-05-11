5af8e4cd070f3ec4f9b002546b065927d72f966c
Num-21 <leocalvin.m@gmail.com>
2022-05-11 10:57:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// console.log('hi mon');
+
+const btn = document.querySelector('.btn');
+const overlay = document.querySelector('.overlay');
+const popup = document.querySelector('.popup');
+// console.log( document.querySelector('.popup'));
+const cross = document.querySelector('.cross');
+const firstName = document.querySelector('.name');
+const maincontent = document.querySelector('.main');
+
+btn.addEventListener('click', function () {
+    
+    if (firstName.value !== "") {
+        
+        
+        overlay.classList.toggle('hidden');
+        popup.classList.toggle('hidden');
+        maincontent.textContent = `Hey ${firstName.value}`;
+    }
+    
+
+
+});
+cross.addEventListener('click',function(){
+
+    overlay.classList.toggle('hidden')
+    popup.classList.toggle('hidden')
+    
+
+
+});
